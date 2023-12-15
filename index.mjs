ca6174800787ee1227d7481ee15c0ca05aa9c36d
mecronorm <alessandropaap@hotmail.com>
2023-12-15 11:31:34 +0100

    Make the food list bigger

--- index.mjs
+++ index.mjs
@@ -5,7 +5,6 @@ import dotenv from "dotenv"
 dotenv.config()
 
 const app = express()
-const PORT = 3000
 
 app.use(express.json())
 
@@ -14,7 +13,7 @@ function authentication(req,res,next) {
     const providedKey = req.headers['x-api-key']
     
     if (!providedKey || providedKey !== process.env.API_KEY) {
-        return res.status(401).json({error: 'Unauthorized access'})
+        return res.status(403).json({error: 403, msg:'Unauthorized access'})
     }
     
     next()
@@ -31,18 +30,27 @@ app.get("/favorites", (req,res)=>{
     res.send(favorites)
 })
 
-app.get("/:name", (req, res)=>{
+app.get("/:name", (req,res,next)=>{
     const name = req.params.name
     console.log(name);
     const foodItem = food.find(item => item.name.toLowerCase() === name.toLowerCase())
     if (!foodItem) {
-        return res.status(404).json({ message: 'Food item not found' });
+        next()
       }
     res.send(foodItem)
 })
 
+app.get("/:cuisine", (req, res)=>{
+    const cuisine = req.params.cuisine
+    const foodItems = food.filter(item => item.cuisine.toLowerCase() === cuisine.toLowerCase())
+    if (!foodItems[0]) {
+        return res.status(404).json({ message: 'Food item not found' });
+      }
+    res.send(foodItems)
+})
 
 
-app.listen(PORT, ()=> {
+
+app.listen(process.env.PORT, ()=> {
     console.log("server is running on http://localhost:3000");
 })
